97b4bced7c2d6a48ad8262aacefd832df6536b8a
kapsi1 <kapsiakk@gmail.com>
2025-03-13 18:43:29 +0100

    remove board

--- src/snake.ts
+++ src/snake.ts
@@ -1,82 +1,74 @@
-//TODO zrobić węża jako path zamiast kwadratów
-//płynna animacja
+// TODO
+// zrobić węża jako path zamiast kwadratów
+// płynna animacja
+import { Tile, TileType, TilePosition, Direction, GameState } from './types';
 
-import { TileType, TilePosition, Direction, GameState } from "./types";
-
-const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d", { alpha: false }) as CanvasRenderingContext2D;
-
-const CANVAS_WIDTH_PX = 602;
-const CANVAS_HEIGHT_PX = 602;
+const CANVAS_WIDTH_PX = 600;
+const CANVAS_HEIGHT_PX = 600;
 const BOARD_SIZE = 15;
-const WALLS_WRAP = false;
-const SHOW_GRID = false;
-const TICK_SPEED_MS = 250;
+const WALLS_WRAP = true;
 const TILE_WIDTH_PX = Math.floor(CANVAS_WIDTH_PX / BOARD_SIZE);
 const TILE_HEIGHT_PX = Math.floor(CANVAS_HEIGHT_PX / BOARD_SIZE);
+const BACKGROUND_COLOR = 'black';
 
-const keyboardListener = (state: GameState) => (event: KeyboardEvent) => {
-  if (state.isGameOver) return;
-  gameState.prevDirection = gameState.direction;
-  switch (event.key) {
-    case "w":
-      gameState.direction = Direction.Up;
-      break;
-    case "s":
-      gameState.direction = Direction.Down;
-      break;
-    case "d":
-      gameState.direction = Direction.Right;
-      break;
-    case "a":
-      gameState.direction = Direction.Left;
-      break;
-  }
-  // tick(state);
+const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+canvas.setAttribute('width', (CANVAS_WIDTH_PX * window.devicePixelRatio).toString());
+canvas.style.setProperty('width', CANVAS_WIDTH_PX.toString() + 'px');
+canvas.setAttribute('height', (CANVAS_HEIGHT_PX * window.devicePixelRatio).toString());
+canvas.style.setProperty('height', CANVAS_HEIGHT_PX.toString() + 'px');
+const ctx = canvas.getContext('2d', { alpha: false }) as CanvasRenderingContext2D;
+ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+ctx.textBaseline = 'middle';
+ctx.textAlign = 'center';
+
+const randomPos = () => Math.floor(Math.random() * BOARD_SIZE);
+const state: GameState = {
+  direction: Direction.Right,
+  prevDirection: Direction.Right,
+  snake: [
+    [3, 4],
+    [2, 4],
+    [1, 4],
+  ],
+  fruit: null,
+  isGameOver: false,
 };
+state.fruit = getNewFruitPosition();
+let lastTimestamp = 0;
 
-function getColor(tileIndex: number, totalTiles: number, isGameOver: boolean): string {
+function getTileColor(tileIndex: number, totalTiles: number, isGameOver: boolean): string {
   const opacity = Math.max(0.25, 1 - tileIndex / totalTiles);
   if (isGameOver) return `rgba(139, 0, 0, ${opacity})`;
   return `rgba(255, 255, 255, ${opacity})`;
 }
 
-function draw(state: GameState) {
-  const { board } = state;
-  ctx.strokeStyle = "darkgray";
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, CANVAS_WIDTH_PX, CANVAS_HEIGHT_PX);
-  ctx.strokeRect(0, 0, CANVAS_WIDTH_PX, CANVAS_HEIGHT_PX);
+function drawDebugText(x: number, y: number) {
+  const xPos = x * TILE_WIDTH_PX;
+  const yPos = y * TILE_HEIGHT_PX;
+  ctx.fillStyle = 'lightgray';
+  ctx.font = `10px serif`;
+  ctx.fillText(`${x}, ${y}`, xPos + 20, yPos + 20);
+}
 
-  for (let x = 0; x < board.length; x++) {
-    for (let y = 0; y < board.length; y++) {
-      const tile = board[x][y];
-      const xPos = x * TILE_WIDTH_PX + 1;
-      const yPos = y * TILE_HEIGHT_PX + 1;
-      ctx.fillStyle = tile.color as string;
+function clearTile(x: number, y: number) {
+  const xPos = x * TILE_WIDTH_PX;
+  const yPos = y * TILE_HEIGHT_PX;
+  ctx.fillStyle = BACKGROUND_COLOR;
+  ctx.fillRect(xPos, yPos, TILE_WIDTH_PX, TILE_HEIGHT_PX);
+  // drawDebugText(x, y);
+}
 
-      switch (tile.type) {
-        case TileType.Empty:
-          if (SHOW_GRID) ctx.strokeRect(xPos, yPos, TILE_WIDTH_PX, TILE_HEIGHT_PX);
-          break;
+function drawTile(x: number, y: number, tile: Tile) {
+  const xPos = x * TILE_WIDTH_PX;
+  const yPos = y * TILE_HEIGHT_PX;
+  ctx.fillStyle = tile.color as string;
+  ctx.fillRect(xPos, yPos, TILE_WIDTH_PX, TILE_HEIGHT_PX);
 
-        case TileType.Head:
-        case TileType.Body:
-          ctx.fillRect(xPos, yPos, TILE_WIDTH_PX, TILE_HEIGHT_PX);
-          break;
-
-        case TileType.Fruit:
-          ctx.fillRect(xPos, yPos, TILE_WIDTH_PX, TILE_HEIGHT_PX);
-          ctx.font = `${TILE_WIDTH_PX / 1.5}px serif`;
-          ctx.textBaseline = "middle";
-          ctx.textAlign = "center";
-          const fontXPos = xPos + TILE_WIDTH_PX / 2;
-          const fontYPos = yPos + TILE_HEIGHT_PX / 2;
-          ctx.fillText("🍒", fontXPos, fontYPos);
-          if (SHOW_GRID) ctx.strokeRect(xPos, yPos, TILE_WIDTH_PX, TILE_HEIGHT_PX);
-          break;
-      }
-    }
+  if (tile.type === TileType.Fruit) {
+    ctx.font = `${TILE_WIDTH_PX / 1.5}px serif`;
+    const fontXPos = xPos + TILE_WIDTH_PX / 2;
+    const fontYPos = yPos + TILE_HEIGHT_PX / 2;
+    ctx.fillText('🍒', fontXPos, fontYPos);
   }
 }
 
@@ -85,43 +77,66 @@ function isSameTile(a: TilePosition | null, b: TilePosition | null) {
   return a[0] === b[0] && a[1] === b[1];
 }
 
-function getBoard(state: GameState) {
-  const { snake, fruit, isGameOver, board, lastSnakeSegment } = state;
-  if (lastSnakeSegment) {
-    board[lastSnakeSegment[0]][lastSnakeSegment[1]] = {
-      type: TileType.Empty,
-      color: "black",
+function drawTiles() {
+  const { snake, fruit, isGameOver } = state;
+  snake.forEach((segment: TilePosition, index: number) => {
+    const tile: Tile = {
+      type: TileType.Snake,
+      color: getTileColor(index, snake.length, isGameOver),
     };
-  }
-  snake.forEach((segment, index) => {
-    board[segment[0]][segment[1]] = {
-      type: index === 0 ? TileType.Head : TileType.Body,
-      color: getColor(index, snake.length, isGameOver),
-    };
+    clearTile(segment[0], segment[1]);
+    drawTile(segment[0], segment[1], tile);
   });
-  if (fruit) board[fruit[0]][fruit[1]] = { type: TileType.Fruit, color: "black" };
-  return board;
+  drawTile(fruit![0], fruit![1], { type: TileType.Fruit, color: BACKGROUND_COLOR });
 }
 
-const randomPos = () => Math.floor(Math.random() * BOARD_SIZE);
-
-function getNewFruitPosition(state: GameState) {
+function getNewFruitPosition() {
   const newPosition: TilePosition = [randomPos(), randomPos()];
-
   for (let i = 0; i < state.snake.length; i++) {
     if (isSameTile(state.snake[i], newPosition)) {
-      return getNewFruitPosition(state);
+      return getNewFruitPosition();
     }
   }
   return newPosition;
 }
 
-function tick(state: GameState, timestamp: number, lastTimestamp: number) {
+window.addEventListener('keydown', (event: KeyboardEvent) => {
+  if (state.isGameOver) return;
+  state.prevDirection = state.direction;
+  switch (event.key) {
+    case 'Escape':
+      state.isGameOver = true;
+      break;
+    case 'w':
+      state.direction = Direction.Up;
+      break;
+    case 's':
+      state.direction = Direction.Down;
+      break;
+    case 'd':
+      state.direction = Direction.Right;
+      break;
+    case 'a':
+      state.direction = Direction.Left;
+      break;
+  }
+  // Don't allow reversing into body
+  if (
+    (state.prevDirection === Direction.Up && state.direction === Direction.Down) ||
+    (state.prevDirection === Direction.Down && state.direction === Direction.Up) ||
+    (state.prevDirection === Direction.Left && state.direction === Direction.Right) ||
+    (state.prevDirection === Direction.Right && state.direction === Direction.Left)
+  ) {
+    state.direction = state.prevDirection;
+  }
+});
+
+function tick(timestamp: number) {
   const deltaT = timestamp - lastTimestamp;
   // console.log("last", lastTimestamp, "current", timestamp, "delta", deltaT);
-  // if (deltaT < 100 && timestamp < 5000) {
+  // if (deltaT < 100) {
   if (deltaT < 100) {
-    requestAnimationFrame((timestamp) => tick(gameState, timestamp, lastTimestamp));
+    requestAnimationFrame(tick);
     return;
   }
   lastTimestamp = timestamp;
@@ -167,67 +182,44 @@ function tick(state: GameState, timestamp: number, lastTimestamp: number) {
       break;
   }
 
-  // don't allow reversing into the body
+  // Don't allow reversing into body
+  // Keyboard listener should catches of those cases, but it can still happen when keys are pressed quickly
   if (isSameTile(newHead, snake[1])) {
     state.direction = state.prevDirection;
+    requestAnimationFrame(tick);
     return;
   }
 
-  // check for collision with body
-  for (let i = 0; i < snake.length; i++) {
-    if (isSameTile(snake[i], newHead)) {
+  // Check for collision with body
+  // Head can't collide with first 2 body tiles
+  for (let i = 3; i < snake.length; i++) {
+    if (isSameTile(newHead, snake[i])) {
       state.isGameOver = true;
     }
   }
 
   if (state.isGameOver) {
-    state.board = getBoard(state);
-    draw(state);
+    drawTiles();
     return;
   }
 
   let ateFruit = false;
   if (isSameTile(newHead, fruit)) {
     ateFruit = true;
-    state.snake.splice(1, 0, snake[0]);
-    state.fruit = getNewFruitPosition(state);
+    state.snake.splice(1, 0, [snake[0][0], snake[0][1]]);
+    state.fruit = getNewFruitPosition();
   }
 
-  state.lastSnakeSegment = snake[snake.length - 1];
+  const lastSnakeSegment = snake[snake.length - 1];
+  clearTile(lastSnakeSegment[0], lastSnakeSegment[1]);
   state.snake = snake.map((segment, index) => {
     if (index === 0) return newHead;
     if (ateFruit) return segment;
     return snake[index - 1];
   });
-
-  state.board = getBoard(state);
-  // console.log(state.snake);
-  draw(state);
-  // if (timestamp < 5000) 
-    requestAnimationFrame((timestamp) => tick(gameState, timestamp, lastTimestamp));
+  drawTiles();
+  requestAnimationFrame(tick);
 }
 
-const gameState: GameState = {
-  board: new Array(BOARD_SIZE)
-    .fill(null)
-    .map(() => new Array(BOARD_SIZE).fill({ type: TileType.Empty, color: "black" })),
-  direction: Direction.Right,
-  prevDirection: Direction.Right,
-  snake: [
-    [3, 4],
-    [2, 4],
-    [1, 4],
-  ],
-  lastSnakeSegment: null,
-  fruit: null,
-  isGameOver: false,
-};
-gameState.fruit = getNewFruitPosition(gameState);
-window.addEventListener("keydown", keyboardListener(gameState));
-let lastTimestamp = 0;
-// const intervalId = setInterval(() => {
-//   if (gameState.isGameOver) clearInterval(intervalId);
-//   else tick(gameState);
-// }, TICK_SPEED_MS);
-
-requestAnimationFrame((timestamp) => tick(gameState, timestamp, lastTimestamp));
+drawTiles();
+requestAnimationFrame(tick);
--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,30 @@
 enum TileType {
-  Empty = 'empty',
-  Body = 'body',
-  Head = 'head',
+  Snake = 'snake',
   Fruit = 'fruit',
 }
 
 enum Direction {
-  Left = "left",
-  Up = "up",
-  Right = "right",
-  Down = "down",
+  Left = 'left',
+  Up = 'up',
+  Right = 'right',
+  Down = 'down',
 }
 
 type TilePosition = [number, number];
+
 type Tile = {
-  type: TileType,
-  color: string
-}
+  type: TileType;
+  color: string;
+};
+
 type Snake = TilePosition[];
-type Board = Tile[][];
 
 type GameState = {
-  board: Board;
   direction: Direction;
   prevDirection: Direction;
   snake: Snake;
-  lastSnakeSegment: TilePosition | null;
-  fruit: TilePosition | null,
-  isGameOver: boolean
+  fruit: TilePosition | null;
+  isGameOver: boolean;
 };
 
-export { Tile, TileType, TilePosition, Direction, Snake, Board, GameState };
+export { Tile, TileType, TilePosition, Direction, Snake, GameState };
