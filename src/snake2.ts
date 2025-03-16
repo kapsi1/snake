878ba8a64119b1028675412f607ce309e36b3982
kapsi1 <kapsiakk@gmail.com>
2025-03-16 04:04:09 +0100

    arcTo

--- src/snake2.ts
+++ src/snake2.ts
@@ -16,20 +16,49 @@ const CELL_SIZE_PX = Math.floor(CANVAS_WIDTH_PX / BOARD_SIZE);
 const BACKGROUND_COLOR = 'black';
 const DOT_RADIUS = 5;
 const SPEED = CELL_SIZE_PX / 10; // px/s
+const COLOR = '#0085ee';
 
+const debugEl = document.querySelector('#debug') as HTMLDivElement;
+const prevHeadCell: GridPosition = { x: -1, y: -1 };
+// const snake: GridPosition[] = [
+//   { x: 0, y: 0 },
+//   { x: 0, y: 1 },
+//   { x: 1, y: 1 },
+//   { x: 1, y: 0 },
+//   { x: 2, y: 0 },
+//   { x: 2, y: 1 },
+//   { x: 2, y: 2 },
+//   { x: 1, y: 2 },
+//   { x: 0, y: 2 }
+// ];
+// const snake: GridPosition[] = [
+//   { x: 0, y: 0 },
+//   { x: 1, y: 0 },
+//   { x: 2, y: 0 },
+//   { x: 2, y: 1 },
+//   { x: 1, y: 1 },
+//   { x: 0, y: 1 },
+//   { x: 0, y: 2 },
+//   { x: 1, y: 2 },
+//   { x: 2, y: 2 },
+// ];
 const snake: GridPosition[] = [
-  { x: 0, y: 1 },
-  { x: 1, y: 1 },
+  { x: 0, y: 0 },
   { x: 1, y: 0 },
   { x: 2, y: 0 },
   { x: 2, y: 1 },
+  { x: 2, y: 2 },
+  { x: 1, y: 2 },
+  { x: 0, y: 2 },
+  { x: 0, y: 1 },
+  { x: 1, y: 1 },
 ];
-const debugEl = document.querySelector('#debug') as HTMLDivElement;
-const prevCell: GridPosition = { x: -1, y: -1 };
-// const currentCell: GridPosition = { x: 0, y: 1 };
-const currentCell: GridPosition = { x: snake[snake.length - 1].x, y: snake[snake.length - 1].y };
-const head: Position = { ...getCellCenter(currentCell) };
+const headCell: GridPosition = { x: snake[0].x, y: snake[0].y };
 let prevHead: Position | null = null;
+const center = getCellCenter(headCell);
+// const head: Position = { x: center.x, y: center.y };
+const head: Position = { x: center.x - 50, y: center.y };
+// const head: Position = { x: center.x, y: center.y + 75 };
 let lastTimestamp: number | null = null;
 let pause = false;
 // let direction = Direction.Right;
@@ -46,13 +75,14 @@ function clearBackground() {
 }
 
 function drawDebug() {
-  ctx.fillStyle = '#3b3b3b';
+  const color = '#b6b6b6';
+  ctx.fillStyle = color;
   for (let x = 0; x < BOARD_SIZE; x++) {
     for (let y = 0; y < BOARD_SIZE; y++) {
       const center = getCellCenter({ x, y });
       ctx.fillText('(' + x + ', ' + y + ')', center.x, center.y - 10); // grid positions
       ctx.fillText('(' + center.x + ', ' + center.y + ')', center.x, center.y + 10); // pixel positions
-      drawDot(center, '#3b3b3b', 2);
+      drawDot(center, color, 2);
     }
   }
   // if (prevHead) {
@@ -101,7 +131,7 @@ function roundPos(pos: Position): Position {
 }
 
 function drawDot(pos: Position, color?: string, radius = DOT_RADIUS) {
-  ctx.fillStyle = color || '#0085ee';
+  ctx.fillStyle = color || 'white';
   ctx.beginPath();
   ctx.arc(pos.x, pos.y, radius, 0, Math.PI * 2);
   ctx.fill();
@@ -128,45 +158,90 @@ function drawSnake() {
 
   // arcTo()
   //TODO calculate
-  const offset = 50;
+  const lineOffset = 50;
   const radius = 50;
-  let center: Position;
-  let nextCenter: Position | null;
-  let nextNextCenter: Position | null;
+  let startPoint: Position;
+  let midPoint: Position | null;
+  let endPoint: Position | null;
+  const headCellCenter = getCellCenter(headCell);
+  // Offset from head position to the center of cell
+  const headOffset = { x: head.x - headCellCenter.x, y: head.y - headCellCenter.y };
+  console.log('head', head, 'headCellCenter', headCellCenter, 'headOffset', headOffset);
   // ctx.lineWidth = 5;
   ctx.lineWidth = CELL_SIZE_PX / 4;
-  // ctx.strokeStyle = '#0085ee';
-  const colors = ['red', 'green', 'blue', 'yellow', 'red'];
-  for (let i = 0; i < snake.length; i++) {
-    ctx.strokeStyle = colors[i];
-    center = getCellCenter(snake[i]);
-    nextCenter = i < snake.length - 1 ? getCellCenter(snake[i + 1]) : null;
-    nextNextCenter = i < snake.length - 2 ? getCellCenter(snake[i + 2]) : null;
+  // ctx.lineWidth = CELL_SIZE_PX / 8;
+  ctx.strokeStyle = COLOR;
 
-    let newX = center.x;
-    let newY = center.y;
-    if (i > 0) {
-      if (nextCenter && nextCenter.x === center.x) {
-        // Moving vertically
-        newY += nextCenter.y > center.y ? offset : -offset;
-      } else if (nextCenter && center.y === nextCenter.y) {
-        // Moving horizontally
-        newX += nextCenter.x > center.x ? offset : -offset;
+  // Head and first point
+  // ctx.beginPath();
+  // ctx.moveTo(head.x, head.y);
+  // console.log('moveTo', head);
+  // const center = getCellCenter(snake[1]);
+  // ctx.lineTo(center.x, center.y);
+  // console.log('lineTo', center);
+  // ctx.stroke();
+
+  const colors = ['darkred', 'green', 'blue', 'orange', 'brown', 'darkred', 'green', 'blue', 'orange', 'brown'];
+  let isCorner = false;
+
+  for (let i = 0; i < snake.length - 1; i++) {
+    ctx.strokeStyle = colors[i];
+    startPoint = getCellCenter(snake[i]);
+    midPoint = getCellCenter(snake[i + 1]);
+    if (i < snake.length - 2) endPoint = getCellCenter(snake[i + 2]);
+    else endPoint = { x: midPoint.x, y: midPoint.y };
+    console.group(i, colors[i]);    
+
+    // let startX = startPoint.x;
+    // let startY = startPoint.y;
+    // let endX = endPoint.x;
+    // let endY = endPoint.y;
+    // console.log('startX', startX, 'startY', startY);
+
+    // Adjust ends of the snake
+    if (i === 0) {
+      startPoint.x += headOffset.x;
+      startPoint.y += headOffset.y;
+    } else if (i === snake.length - 2) {
+      endPoint.x += headOffset.x;
+      midPoint.x = endPoint.x;
+      endPoint.y += headOffset.y;
+      midPoint.y = endPoint.y;
+    }
+
+    // If this segment starts on a corner, offset the start point,
+    // so it doesn't go outside of the rounded corner line
+    if (isCorner) {
+      if (midPoint.y === startPoint.y) {
+        // Vertical -> horizontal
+        startPoint.x += midPoint.x > startPoint.x ? lineOffset : -lineOffset;
+        // console.log('startX2', startX);
+      } else {
+        // Horizontal -> vertical
+        startPoint.y += midPoint.y > startPoint.y ? lineOffset : -lineOffset;
+        // console.log('startY2', startY);
       }
     }
-    ctx.beginPath();
-    ctx.moveTo(newX, newY);
-    console.log(i, 'moveTo', { x: newX, y: newY });
+    console.log('start', startPoint, 'mid', midPoint, 'end', endPoint, 'isCorner', isCorner);
+    isCorner = startPoint.x !== endPoint.x && startPoint.y !== endPoint.y;
 
-    if (nextCenter && nextNextCenter) {
-      ctx.arcTo(nextCenter.x, nextCenter.y, nextNextCenter.x, nextNextCenter.y, radius);
-      console.log(i, colors[i], 'arcTo', nextCenter, nextNextCenter);
-    } else if (nextCenter) {
-      console.log(i, colors[i], 'lineTo', nextCenter);
-      ctx.lineTo(nextCenter.x, nextCenter.y);
-    }
-    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(startPoint.x, startPoint.y);
+    console.log('moveTo', startPoint);
+
+    // if (i < snake.length - 1) {
+    ctx.arcTo(midPoint.x, midPoint.y, endPoint.x, endPoint.y, radius);
+    console.log('arcTo', midPoint, endPoint);
+    // } else {
+    // console.log(i, colors[i - 2], 'lineTo', midPoint');
+    // ctx.lineTo(midPoint'.x, midPoint'.y);
+    // console.log(i, colors[i - 2], 'lineTo', endPoint);
+    // ctx.lineTo(endPoint.x + headOffset.x, endPoint.y + headOffset.y);
+    // }
+    ctx.stroke();    
+    console.groupEnd();
   }
+  drawDot(endPoint!, 'red');
 
   // quadraticCurveTo()
   // let cellCenter: Position;
@@ -200,18 +275,18 @@ function tick(timestamp: number) {
   const deltaPx = (SPEED * deltaT) / 1000;
   lastTimestamp = timestamp;
 
-  const cc = getCurrentCell();
-  currentCell.x = cc.x;
-  currentCell.y = cc.y;
+  const c = getCurrentCell();
+  headCell.x = c.x;
+  headCell.y = c.y;
 
-  if (currentCell.x !== prevCell.x || currentCell.y !== prevCell.y) {
-    prevCell.x = currentCell.x;
-    prevCell.y = currentCell.y;
+  // Moved to a new cell
+  if (headCell.x !== prevHeadCell.x || headCell.y !== prevHeadCell.y) {
+    prevHeadCell.x = headCell.x;
+    prevHeadCell.y = headCell.y;
     distanceTravelled = 0;
   }
 
   distanceTravelled += deltaPx;
-
   if (direction === Direction.Right) head.x += deltaPx;
   if (direction === Direction.Left) head.x -= deltaPx;
   if (direction === Direction.Up) head.y -= deltaPx;
@@ -222,7 +297,7 @@ function tick(timestamp: number) {
   // and add distance travelled in this cell
   if (prevDirection) {
     prevHead = { x: head.x, y: head.y };
-    const cellCenter = getCellCenter({ x: currentCell.x, y: currentCell.y });
+    const cellCenter = getCellCenter({ x: headCell.x, y: headCell.y });
 
     switch (direction) {
       case Direction.Right:
